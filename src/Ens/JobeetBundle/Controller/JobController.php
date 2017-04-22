b6f6ade77aef104c62e9c65f960b8b2707d60b58
dkaufman <kaufman.dmitry@gmail.com>
2017-04-22 17:26:58 +0700

    Point 8. Translations

--- src/Ens/JobeetBundle/Controller/JobController.php
+++ src/Ens/JobeetBundle/Controller/JobController.php
@@ -21,9 +21,6 @@ class JobController extends Controller
      */
     public function indexAction(Request $request)
     {
-        var_dump($this->get('translator')->trans('Jobeet - Your best job board'));
-        die();
-
         $em = $this->getDoctrine()->getManager();
 
         $categories = $em->getRepository('EnsJobeetBundle:Category')->getWithJobs();
